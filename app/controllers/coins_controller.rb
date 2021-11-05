fe5bb00b6e8750e1d401b0af8188d01a5d3fdf48
Juan Carlos Meza <juancmezat@gmail.com>
2021-11-04 23:58:03 -0700

    Coins controller

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -5,4 +5,16 @@ class CoinsController < ApplicationController
       render json: coins
   end
 
+  def create
+    Coin.create(params)
+  end
+
+  def show
+    Coin.find_by(name: params[:name])
+  end
+
+  def destroy
+    Coin.find(params[:id]).destroy
+  end
+
 end
